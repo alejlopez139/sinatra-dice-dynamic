5e3517c9eb49c5db15852e5a3fe2cea3b0e627bc
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-09-20 19:52:52 +0000

    finished multiple parameters

--- app.rb
+++ app.rb
@@ -65,13 +65,15 @@ get("/dynamic/50/6") do
   erb(:flexible)
 end
 
-get("/dynamic/:number_of_dice/6") do
+get("/dynamic/:number_of_dice/:how_many_sides") do
   @num_dice = params.fetch("number_of_dice").to_i
+
+  @sides = params.fetch("how_many_sides").to_i
 
   @rolls = []
 
   @num_dice.times do
-    die = rand(1..6)
+    die = rand(1..@sides)
 
     @rolls.push(die)
   end
